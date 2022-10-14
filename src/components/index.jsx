0b85a543633f87efd06cdbe19735e61940ec58f1
ibrar hussain <ibrarawan292@gmail.com>
2022-10-14 11:30:22 -0700

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+export { default as Button } from './Button/Button';
+export { default as ThemeSetting } from './ThemeSetting/ThemeSetting';
+export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Footer } from './Footer/Footer';
+export { default as Cart } from './Cart/Cart';
+export { default as Chat } from './Chart/Chart';
+export { default as Notification } from './Notification/Notification';
+export { default as UserProfile } from './UserProfile/UserProfile';
+export { default as SparkLine } from './Charts/SparkLine/SparkLine';
+export { default as LineChart } from './Charts/LineChart/LineChart';
+export { default as Stacked } from './Charts/Stacked/Stacked';
+export { default as Pie } from './Charts/Pie/Pie';
+export { default as ChartsHeader } from './ChartsHeader/ChartsHeader';
+export { default as Header } from './Header/Header';
